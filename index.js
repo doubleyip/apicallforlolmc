2136a12f88f08838e4a4e3009bfbfc5065e553c4
doubleyip <syip428@gmail.com>
2017-11-26 15:33:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var async = require('async');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 app.set('view engine', 'handlebars');
-
+app.get('/',function(req,res){
+	res.render('index');
+});
 
 app.get('/sumSearch', function(req, res) {
   var data = {};

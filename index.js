158b1795eeb1fe43f56c51fb3a628263a1beaf2e
doubleyip <syip428@gmail.com>
2017-11-26 15:30:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var app = express();
+var exphbs = require('express-handlebars');
+var request = require('request');
+var async = require('async');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+
+
+app.get('/sumSearch', function(req, res) {
+  var data = {};
+  var server = 'na';
+  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var sumSearch = req.query.name;
+  var URL = 'https://'+server+'1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + sumSearch + '?api_key=' + apiKey;
+  console.log(URL);
+
+  async.waterfall([
+    function(callback) {
+      request(URL, function(err, response, body) {
+        if(!err && response.statusCode == 200) {
+          var json = JSON.parse(body);
+		  newjson=json
+          callback(null, data);
+		  console.log(data.name);
+		  console.log(data.id);
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  ],
+  function(err, data) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    /*res.render('index', {
+      info: json
+	  
+    })*/
+	res.status(404).json(newjson);
+  });
+});
+
+var port = Number(process.env.PORT || 3000);
+app.listen(port);

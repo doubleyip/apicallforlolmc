c0ecf9d86e5f1c9a5f4a422c25cdd9f441aeeb71
doubleyip <syip428@gmail.com>
2017-12-11 12:44:55 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -153,5 +153,41 @@ app.get('/getTimeline', function(req, res) {
   });
 });
 
+
+app.get('/getLeague', function(req, res) {
+  var data = {};
+  var server = 'na';
+  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var recentMatch = req.query.rmatch;
+  var URL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/' + recentMatch + '?api_key=RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  console.log(URL);
+
+  async.waterfall([
+    function(callback) {
+      request(URL, function(err, response, body) {
+        if(!err && response.statusCode == 200) {
+          var json = JSON.parse(body);
+		  newjson=json
+          callback(null, data);
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  ],
+  function(err, data) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    /*res.render('index', {
+      info: json
+	  
+    })*/
+	res.status(200).json(newjson);
+  });
+});
+
 var port = Number(process.env.PORT || 3000);
 app.listen(port);

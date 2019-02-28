f88b5dbf79905c604f2d4ee8e134333de60f4e65
doubleyip <syip428@gmail.com>
2019-02-28 14:31:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var exphbs = require('express-handlebars');
 var request = require('request');
 var async = require('async');
 var cors=require('cors');
+var apiKey = 'RGAPI-93766d03-7e34-4b78-b4df-eba3f414757f';
 
 app.use(cors({origin: '*'}));		//enable allow access origin control
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -16,7 +17,7 @@ app.get('/',function(req,res){
 app.get('/sumSearch', function(req, res) {	//call for summoner searching	
   var data = {};				//?name="name you want to search"
   var server = 'na';
-  var apiKey = 'RGAPI-93766d03-7e34-4b78-b4df-eba3f414757f';
+  //var apiKey = 'RGAPI-93766d03-7e34-4b78-b4df-eba3f414757f';
   var sumSearch = req.query.name;
   var URL = 'https://'+server+'1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + sumSearch + '?api_key=' + apiKey;
   console.log(URL);
@@ -51,9 +52,9 @@ app.get('/sumSearch', function(req, res) {	//call for summoner searching
 app.get('/matchSearch', function(req, res) {		//api call to search for a match
   var data = {};					//?id="id of match to search
   var server = 'na';
-  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  //var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
   var accountID = req.query.id;
-  var URL = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/'+ accountID + '/recent?api_key=RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var URL = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/'+ accountID + '/recent'+'?api_key=' + apiKey;
   console.log(URL);
 
   async.waterfall([
@@ -86,9 +87,9 @@ app.get('/matchSearch', function(req, res) {		//api call to search for a match
 app.get('/recentSearch', function(req, res) {		//search recent matches
   var data = {};					//?rmatch="match to search"
   var server = 'na';
-  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  //var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
   var recentMatch = req.query.rmatch;
-  var URL = 'https://na1.api.riotgames.com/lol/match/v3/matches/' + recentMatch + '?api_key=RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var URL = 'https://na1.api.riotgames.com/lol/match/v3/matches/' + recentMatch + '?api_key=' + apiKey;
   console.log(URL);
 
   async.waterfall([
@@ -121,9 +122,9 @@ app.get('/recentSearch', function(req, res) {		//search recent matches
 app.get('/getTimeline', function(req, res) {		//call to get timeline
   var data = {};					//?rmatch="match you want to get timeline of"
   var server = 'na';
-  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  //var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
   var recentMatch = req.query.rmatch;
-  var URL = 'https://na1.api.riotgames.com/lol/match/v3/timelines/by-match/' + recentMatch + '?api_key=RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var URL = 'https://na1.api.riotgames.com/lol/match/v3/timelines/by-match/' + recentMatch + '?api_key=' + apiKey;
   console.log(URL);
 
   async.waterfall([
@@ -157,9 +158,9 @@ app.get('/getTimeline', function(req, res) {		//call to get timeline
 app.get('/getLeague', function(req, res) {		//api call to get league of player
   var data = {};					//?id="id of player you want to get league of"
   var server = 'na';
-  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  //var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
   var sumID = req.query.id;
-  var URL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/' + sumID + '?api_key=RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var URL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/' + sumID + '?api_key=' + apiKey;
   console.log(URL);
 
   async.waterfall([
@@ -192,9 +193,9 @@ app.get('/getLeague', function(req, res) {		//api call to get league of player
 app.get('/getChamp', function(req, res) {		//api to get champ
   var data = {};					//?champID="id of champ you want to search"
   var server = 'na';
-  var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  //var apiKey = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
   var championID = req.query.champID;
-  var URL = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/' + championID + '?locale=en_US&api_key=RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
+  var URL = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/' + championID + '?locale=en_US&'+'&api_key=' + apiKey;
   console.log(URL);
 
   async.waterfall([

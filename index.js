86ed7c681404d39bcc8e6c73d4ce2607cfdf127a
doubleyip <syip428@gmail.com>
2019-10-23 17:03:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var exphbs = require('express-handlebars');
 var request = require('request');
 var async = require('async');
 var cors=require('cors');
-var apiKey = 'RGAPI-5ebafcd2-9cc9-4779-b9f1-a6ec913f49a7';
+var apiKey = 'RGAPI-694e4cc6-d610-4f1d-a871-c243395387c3';
 
 app.use(cors({origin: '*'}));		//enable allow access origin control
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
